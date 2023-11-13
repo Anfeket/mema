ba1d610cd1ca4834229eced9f59763b1132402e9
Anfeket <n5ye.yt@gmail.com>
2023-11-13 14:54:40 +0100

    Working shared state

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::sync::{Arc, Mutex};
 
 use crate::db::*;
 
+#[derive(Clone)]
 struct State {
     database: Arc<Mutex<db::localdb::LocalDatabase>>
 }
@@ -10,7 +11,7 @@ struct State {
 fn main() {
     let meme = Meme::new("anfeket".to_string(), "google.com".to_string(), MediaType::Text);
     let database = db::localdb::LocalDatabase::new("pipik.data".to_string());
-    let mut database = match database {
+    let database = match database {
         Ok(database) => {
             println!("All well and ready!");
             database
@@ -23,6 +24,8 @@ fn main() {
     let state = State{
         database: Arc::new(Mutex::new(database))
     };
+    //let id = database.create_item(meme).unwrap();
+    let mut database = state.database.lock().unwrap();
     let id = database.create_item(meme).unwrap();
     let result = database.get_item(id).unwrap().unwrap();
     let text = format!("Author: {}\nUrl: {}", result.author, result.url);
